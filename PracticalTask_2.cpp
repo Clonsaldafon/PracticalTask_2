611c53b4495afe2e5726d2d82738e696ea0a8ddc
Anton Bezmelnitsin <a.bezmelnitsin.a@yandex.ru>
2023-03-12 20:35:16 +0500

    Ready v0.1

--- PracticalTask_2.cpp
+++ PracticalTask_2.cpp
@@ -2,5 +2,17 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int a, b, c, d, e, x;
+    std::cin >> a >> b >> c >> d >> e;
+
+    __asm {
+        mov eax, a
+        add eax, b
+        mul c
+        sub eax, d
+        div e
+        mov x, eax
+    };
+
+    std::cout << x;
 }
